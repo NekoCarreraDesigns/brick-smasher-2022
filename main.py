29d2d7b98347968d12f0de880f4f8e4c54a66318
NekoCarreraDesigns <denvermaas@gmail.com>
2022-04-01 12:43:02 -0600

    added sprites to main.py

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import pygame
 import sys
 import time
 from settings import *
+from sprites import Player
 
 
 class Game:
     def __init__(self):
-
+        # general setup
         pygame.init()
         self.display_surface = pygame.display.set_mode(
             (screen_width, screen_height))
         pygame.display.set_caption('BrickBasher')
+        # background
         self.bg = self.create_bg()
+        # sprite group
+        self.all_sprites = pygame.sprite.Group()
+        # sprite setup
+        self.player = Player(self.all_sprites)
 
     def create_bg(self):
         bg_original = pygame.image.load(
@@ -23,16 +29,20 @@ class Game:
     def run(self):
         last_time = time.time()
         while True:
+            # delta time
             dt = time.time() - last_time
             last_time = time.time()
-
+            # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
+            # draw frame
             self.display_surface.blit(self.bg, (0, 0))
+            self.all_sprites.draw(self.display_surface)
 
+            # update window
             pygame.display.update()
 
 
