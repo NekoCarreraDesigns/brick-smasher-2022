ebec798e6135000276392ff424d3f02d1f4f12ec
NekoCarreraDesigns <denvermaas@gmail.com>
2022-04-01 11:33:50 -0600

    added settings.py with settings for game

---
+++ settings.py
@@ -0,0 +1,30 @@
+screen_width = 1280
+screen_height = 720
+
+block_map = [
+    '666666666666',
+    '444557755444',
+    '333333333333',
+    '222222222222',
+    '111111111111',
+    '            ',
+    '            ',
+    '            ',
+    '            ']
+
+COLOR_LEGEND = {
+    '1': 'blue',
+    '2': 'green',
+    '3': 'red',
+    '4': 'orange',
+    '5': 'purple',
+    '6': 'bronce',
+    '7': 'grey',
+}
+
+gap_size = 2
+block_height = screen_height / len(block_map) - gap_size
+block_width = screen_width / len(block_map[0]) - gap_size
+top_offset = screen_height // 30
+
+upgrades = ['speed', 'laser', 'heart', 'size']
