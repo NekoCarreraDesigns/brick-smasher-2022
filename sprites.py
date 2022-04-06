7538113d4d9f5a8a4fe134d15b0a0af6e5db213a
NekoCarreraDesigns <denvermaas@gmail.com>
2022-04-06 13:18:28 -0600

    created Ball class with methods for updating, movement, and collision

--- sprites.py
+++ sprites.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from random import choice
 
 
 class Player(pygame.sprite.Sprite):
@@ -12,7 +13,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom=(
             screen_width // 2, screen_height - 20))
         self.direction = pygame.math.Vector2()
-        self.speed = 400
+        self.speed = 300
         self.pos = pygame.math.Vector2(self.rect.topleft)
     # player input for movement
 
@@ -39,3 +40,36 @@ class Player(pygame.sprite.Sprite):
         self.pos.x += self.direction.x * self.speed * dt
         self.rect.x = round(self.pos.x)
         self.screen_constraint()
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, groups, player):
+        super().__init__(groups)
+
+        # collsion objects
+        self.player = player
+
+        # graphics setup
+        self.image = pygame.image.load(
+            './graphics/other/ball.png').convert_alpha()
+
+        # position setup
+        self.rect = self.image.get_rect(midbottom=player.rect.midtop)
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2((choice((1, -1)), -1))
+        self.speed = 400
+
+        # active
+        self.active = False
+
+    def update(self, dt):
+        if self.active:
+
+            if self.direction.magnitude() != 0:
+                self.direction = self.direction.normalize()
+
+            self.pos += self.direction * self.speed * dt
+            self.rect.topleft = (round(self.pos.x), round(self.pos.y))
+        else:
+            self.rect.midbottom = self.player.rect.midtop
+            self.pos = pygame.math.Vector2(self.rect.topleft)
