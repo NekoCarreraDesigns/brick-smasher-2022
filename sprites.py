f11941300114246573adab7e73376b8179e388f9
NekoCarreraDesigns <denvermaas@gmail.com>
2022-04-06 10:50:03 -0600

    added input function for movement

--- sprites.py
+++ sprites.py
@@ -6,9 +6,19 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
-
+        # setup
         self.image = pygame.Surface((screen_width // 10, screen_height // 20))
         self.image.fill('red')
-
+        # position
         self.rect = self.image.get_rect(midbottom=(
             screen_width // 2, screen_height - 20))
+        self.direction = pygame.math.Vector2()
+
+        def input(self):
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.direction.x = -1
+            elif keys[pygame.K_RIGHT]:
+                self.direction.x = 1
+            else:
+                self.direction = 0
